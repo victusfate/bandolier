ada433a18a952a5cedcc04699d6d815faedc77ed
victusfate <messel@gmail.com>
2021-10-14 14:19:46 -0400

    W2-2806 update auth flow to handle fabio routing

--- bandolier/auth.py
+++ bandolier/auth.py
@@ -1,13 +1,16 @@
 import requests
 
 # oauth 
-def get_token(auth_url,client_id,client_secret,scope):
-  r = requests.post(auth_url,json={
-    'client_id': client_id,
-    'grant_type': 'client_credentials',
-    'client_secret': client_secret,
-    'scope': scope
-  })
+def get_token(auth_url,headers,client_id,client_secret,scope):
+  r = requests.post(auth_url,
+    headers=headers,
+    json={
+      'client_id': client_id,
+      'grant_type': 'client_credentials',
+      'client_secret': client_secret,
+      'scope': scope
+    }
+  )
   # print('response',r.json())
   data = r.json()
   token = None
--- test/auth_test.py
+++ test/auth_test.py
@@ -7,16 +7,19 @@ from bandolier import constants
 
 from bandolier import auth
 
-if len(sys.argv) < 6:
-  print('usage: ' + sys.argv[0] + ' <auth_url> <client_id> <client_secret> <scope> <test_url>')
+if len(sys.argv) < 7:
+  print('usage: ' + sys.argv[0] + ' <auth_url> <host_url> <client_id> <client_secret> <scope> <test_url>')
   exit(0)
 
 auth_url      = sys.argv[1]
-client_id     = sys.argv[2]
-client_secret = sys.argv[3]
-scope         = sys.argv[4]
-test_url      = sys.argv[5]
-token,expires_in = auth.get_token(auth_url=auth_url,client_id=client_id,client_secret=client_secret,scope='services')
+host_url      = sys.argv[2]
+client_id     = sys.argv[3]
+client_secret = sys.argv[4]
+scope         = sys.argv[5]
+test_url      = sys.argv[6]
+headers = {}
+headers['Host'] = host_url
+token,expires_in = auth.get_token(auth_url=auth_url,headers=headers,client_id=client_id,client_secret=client_secret,scope='services')
 print('token',token,'expires_in',expires_in)
 
 # lets try a get request
